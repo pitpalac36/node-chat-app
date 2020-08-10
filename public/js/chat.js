84d70baff0de108c626501313d7fda1ba2c9b2cf
Dunca Laura <lauradunca1005@gmail.com>
2020-08-10 11:26:13 +0300

    rendering messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,18 +4,50 @@ const socket = io()
 // server (emit) -> client (receive) -- acknowledgement --> server
 // client (emit) -> server (receive) -- acknowledgement --> client
 
+// elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#share-location')
+const $messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationTemplate = document.querySelector('#url-template').innerHTML
 
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+
+socket.on('locationMessage', (url) => {
+    console.log(url)
+    const html = Mustache.render(locationTemplate, {
+        url
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()  // prevent the browser from refreshing the whole page
+
+    // disable the form once it's been submitted
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
     const message = e.target.elements.message.value
 
     //EVENT ACKNOWLEDGEMENT syntax : event name, data, function that runs when the event is acknowledged
     socket.emit('sendMessage', message, (error) => {
+        // enable
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
         if (error) {
             return console.log(error)
         }
@@ -23,16 +55,21 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     })
 })
 
-document.querySelector('#share-location').addEventListener('click', () => {
+
+$sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('geolocation is not supported by your browser')
     }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude : position.coords.latitude,
             longitude : position.coords.longitude
         }, () => {
             console.log('Location shared!')
+            $sendLocationButton.removeAttribute('disabled')
         })
     })
 })
--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ io.on('connection', (socket) => {
     socket.emit('message', 'Welcome!')
     socket.broadcast.emit('message', 'A new user has joined!')
 
+
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
 
@@ -42,7 +43,7 @@ io.on('connection', (socket) => {
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
         callback()
     })
     
